a3b179fcb8acb9aea3a302bd9dd2b48589e45907
hotcalpis <hokkaru3@gmail.com>
2020-12-06 00:47:20 +0900

    データ取得、表示

--- client/src/js/components/Post.js
+++ client/src/js/components/Post.js
@@ -1,22 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 export default function Post(props) {
     const [posts, setPosts] = useState([]);
-    axios
-        .get('http://localhost:3001/api/posts')
-        .then(result => {
-            setPosts(result.data);
-            console.log(posts);
-        })
-        .catch(() => {
-            console.log('失敗しました');
-        });
 
-    let title;
-    posts.map((row) => 
-        title = row.title
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios
+                .get('http://localhost:3001/api/posts')
+                .then(result => {
+                    setPosts(result.data);
+                    console.log(posts);
+                })
+                .catch(() => {
+                    console.log('失敗しました');
+                });
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+    <div>
+        {posts.map(post => (
+            <h2 style={{textAlign: 'center'}} key={post.id}>
+                {post.title}
+            </h2>
+        ))}
+    </div>
     );
-
-    return <h1>{title}</h1>;
 }
