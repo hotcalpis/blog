713ec189a2521c852d80def8a342dfa6df954585
hotcalpis <hokkaru3@gmail.com>
2020-12-08 00:22:16 +0900

    fontawesome導入

--- client/src/js/components/Home.js
+++ client/src/js/components/Home.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import PostBoard from './PostBoard';
 import Post from './Post';
 
 export default function Home(props) {
 
     return (
-        <Router>
-            <Switch>
-                <Route path={["/public/index.html", "/"]} component={PostBoard} />
-                <Route path="/post/:id" component={Post} />
-            </Switch>
-        </Router>
+        <div>
+            <header>
+                <FontAwesomeIcon icon={faPlus} />
+            </header>
+            <Router>
+                <Switch>
+                    <Route path={["/public/index.html", "/"]} component={PostBoard} />
+                    <Route path="/post/:id" component={Post} />
+                </Switch>
+            </Router>
+        </div>
     );
 }
