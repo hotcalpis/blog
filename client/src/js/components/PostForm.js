3618ef185bdd19b57e7c68d99802a8efbcc76121
hotcalpis <hokkaru3@gmail.com>
2020-12-11 00:32:20 +0900

    レイアウト最低限

--- client/src/js/components/PostForm.js
+++ client/src/js/components/PostForm.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleUp } from '@fortawesome/free-solid-svg-icons';
 
 export default function PostForm(props) {
     const [title, setTitle] = useState([]);
@@ -21,12 +23,26 @@ export default function PostForm(props) {
     }
 
     return (
-        <>
+        <Vertical>
             <label>Title</label>
-            <input value={title} onChange={handleChangeTitle} />
+            <input type='text' value={title} style={{width: '80%'}} onChange={handleChangeTitle} />
             <label>Body</label>
-            <textarea value={body} onChange={handleChangeBody} />
-            <button type='button' onClick={handleSubmit}>送信</button>
-        </>
+            <textarea value={body} rows='5' style={{width: '80%'}} onChange={handleChangeBody} />
+            <FaIcon icon={faArrowAltCircleUp} size='2x' onClick={handleSubmit} />
+        </Vertical>
     )
 }
+
+const Vertical = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-content: space-around;
+    justify-content: center;
+    align-items: center;
+`;
+
+const FaIcon = styled(FontAwesomeIcon)`
+    margin: 0.4em;
+`;
